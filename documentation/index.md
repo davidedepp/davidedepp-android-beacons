392122bd36455ea31ddf45d2e708f3b664d16aaf
Alfonso Davide Pilato <alfonso.pilato@gmail.com>
2015-02-17 17:31:18 +0100

    write documentation for module

--- documentation/index.md
+++ documentation/index.md
@@ -1,33 +1,83 @@
 # janusbeacons Module
 
-## Description
-
-TODO: Enter your module description here
-
-## Accessing the janusbeacons Module
-
-To access this module from JavaScript, you would do the following:
-
-    var janusbeacons = require("com.alfonsojanus.beacons");
-
-The janusbeacons variable is a reference to the Module object.
-
-## Reference
-
-TODO: If your module has an API, you should document
-the reference here.
-
-### janusbeacons.function
-
-TODO: This is an example of a module function.
-
-### janusbeacons.property
-
-TODO: This is an example of a module property.
-
 ## Usage
 
-TODO: Enter your usage example here
+See this example app for usage: TiBeacons Example App
+
+Become an iBeacon:
+
+	var janusbeacons = require("com.alfonsojanus.beacons");
+
+	TiBeacons.addEventListener("advertisingStatus", function(event) {
+    	Ti.API.info(event.status);
+	});
+
+	TiBeacons.startAdvertisingBeacon({
+   		uuid : "00000000-0000-0000-0000-000000000000",
+   		identifier : "TiBeacon Test",
+   		major: 1,
+   		minor: 2
+	});
+
+Start monitoring for iBeacons in one or more regions. 
+
+	TiBeacons.startMonitoringForRegion({
+    	uuid : "00000000-0000-0000-0000-000000000000",
+   	 	identifier : "Test Region 1",
+	});
+
+	TiBeacons.startMonitoringForRegion({
+    	uuid : "00000000-0000-0000-0000-000000000001",
+    	identifier : "Test Region 2 (group-specific)",
+    	major: 1
+	});
+
+	TiBeacons.startMonitoringForRegion({
+    	uuid : "00000000-0000-0000-0000-000000000002",
+   	 	identifier : "Test Region 3 (device-specific)",
+    	major: 1,
+   		minor: 2
+	});
+	
+Listen for region events:
+
+	TiBeacons.addEventListener("enteredRegion", alert);
+	TiBeacons.addEventListener("exitedRegion", alert);
+	TiBeacons.addEventListener("determinedRegionState", alert);
+	
+Start ranging beacons in a region. This takes takes more energy and will report the approximate distance of the device to the beacon.
+
+	TiBeacons.startRangingForBeacons({
+    	uuid : "00000000-0000-0000-0000-000000000002",
+    	identifier : "Test Region",
+ 	   	major: 1, //optional
+   	 	minor: 2 //optional
+	});
+
+Listen for the range events:
+
+	TiBeacons.addEventListener("beaconRanges", function(event) {
+  	 	alert(event.beacons);
+	});
+
+Or just listen for beacon proximity changes:
+
+	TiBeacons.addEventListener("beaconProximity", function(e){
+   		alert("beacon "+e.major+"/"+e.minor+" is now "+e.proximity);
+	});
+
+
+## Permission and Hardware Status
+
+Find out if bluetooth is on or off (or unauthorized or unsupported or resetting):
+
+	TiBeacons.addEventListener("bluetoothStatus", function(e){
+  	 	if (e.status != "on") {
+      		Ti.API.error("bluetooth is not on");
+   		}
+	});
+
+	TiBeacons.requestBluetoothStatus();
 
 ## Author
 
--- example/app.js
+++ example/app.js
@@ -1,19 +1,112 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
-
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var label = Ti.UI.createLabel();
+var win = Ti.UI.createWindow({backgroundColor:'white'});
+var label = Ti.UI.createLabel({color:'#000000',text:"",top: 100,width: Ti.UI.SIZE, height: Ti.UI.SIZE});
 win.add(label);
+var buttonStop = Titanium.UI.createButton({title: 'Stop Beacon',top: 150,width: Ti.UI.SIZE, height: Ti.UI.SIZE});
+win.add(buttonStop);
+var enter = true;
+var minBeacon = null;
+var MAXPROXIMITY = 1.5;
+var TiBeacons = require('com.alfonsojanus.beacons');
+
+Titanium.API.info("requestBluetoothStatus");
+TiBeacons.addEventListener('bluetoothStatus', bluetoothStatus);
+TiBeacons.requestBluetoothStatus();
+
+buttonStop.addEventListener('click',function(e){
+	Titanium.API.info("Stop Beacon");
+	TiBeacons.stopMonitoringAllRegions();
+	removeListeners();
+});
 win.open();
 
-if (Ti.Platform.name == "android") {
-    var mod = require('com.alfonsojanus.beacons');
-    label.text = "module is => " + mod + "and checkAvailability says: " + mod.checkAvailability();
-} else {
-    label.text = "alfonsojanus.beacons not supported on " + Ti.Platform.name;
+/*Beacon*/
+function beaconStart(){
+	TiBeacons.setAutoRange(true); // è come se si attivase un startRangingForBeacons
+	TiBeacons.startMonitoringForRegion({
+	    uuid : "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
+	    identifier : "Estimote"
+	});
+	addListeners();
+}
+function bluetoothStatus(e){
+	if(e.status == "off"){
+		alert("spento");
+		Ti.API.info("Devi attivare il Bluethoot");
+	}else if(e.status == "on" && enter){
+		setTimeout(function() {// prima di attivare la ricerca dei beacon in Androdid l'ActivityView deve essere già istanziata altrimenti va in crash 
+			beaconStart();
+		}, 500);
+		enter =  !enter;
+	}
+}
+function handleProximity(e){
+	//Ti.API.info("handleProximity"+ JSON.stringify(e));
 }
 
+function updateRanges(e) {
+	var jsonObj = JSON.parse(e.jsoniBeacons);
+	//var minValueProximity = Math.min.apply(Math,jsonObj.map(function(o){return o.proximity;}));
+	Ti.API.info("jsoniBeacons: "+ JSON.stringify(e.jsoniBeacons));
+	for(var i=0;i<jsonObj.length;i++){ //Titanium.API.info(i +": "+ jsonObj[i].minor);
+		if(jsonObj[0].accuracy > -1 && jsonObj[0].accuracy < MAXPROXIMITY){
+			minBeacon = jsonObj[0].minor;
+		}else if(jsonObj[0].accuracy > -1 && jsonObj[0].accuracy > MAXPROXIMITY){
+			minBeacon = null;
+		}
+	}
+	if (minBeacon != null) {
+		label.text = minBeacon + " " + randomString(5);
+	}
+	//Ti.API.info("updateRanges"+ JSON.stringify(e.jsoniBeacons));
+	//var myNewObject = JSON.parse(e.jsoniBeacons);
+	//Titanium.API.info("minor: "+myNewObject[0].minor);
+	//Titanium.API.info("accuracy: "+myNewObject[0].accuracy);
+	//Titanium.API.info("updateRangesmyNewObject:"+myNewObject);
+	/*Titanium.API.info("updateRangesmyNewObjectlength:"+myNewObject.length);
+	Titanium.API.info("updateRangesmyNewObject:"+myNewObject[0]);
+	Titanium.API.info("updateRangesminor:"+myNewObject[0].minor);*/
+}
+function enterRegion(e) {
+	Ti.API.info("exitRegion"+ JSON.stringify(e));//JSON.stringify(e)
+}
+function exitRegion(e) {
+	Ti.API.info("exitRegion"+ JSON.stringify(e));//JSON.stringify(e)
+}
+function determinedRegionState(e){
+	Ti.API.info('determinedRegionState' + JSON.stringify(e));
+}
+function addListeners() {
+	TiBeacons.addEventListener("determinedRegionState", determinedRegionState);
+	
+	TiBeacons.addEventListener("enteredRegion", enterRegion);
+	TiBeacons.addEventListener("exitedRegion", exitRegion);
+	
+	TiBeacons.addEventListener("beaconRanges", updateRanges);
+	TiBeacons.addEventListener("beaconProximity", handleProximity);	
+}
+function removeListeners() {
+	TiBeacons.removeEventListener("determinedRegionState", determinedRegionState);
+	
+	TiBeacons.removeEventListener("enteredRegion", enterRegion);
+	TiBeacons.removeEventListener("exitedRegion", exitRegion);
+	
+	TiBeacons.removeEventListener("beaconRanges", updateRanges);
+	TiBeacons.removeEventListener("beaconProximity", handleProximity);
+}
+function randomString(length) {
+    var result = '';
+    var chars = 'abcdefghijklmnopqrstuvwxyz';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+}
+
+/*
+ * Device how beacon only Nexus 9, Nexus 6, Samsung SM-G900F e SM-G900V
+ 	var isStarted = TiBeacons.startAdvertisingBeacons({
+	    uuid : "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
+	    identifier : "Estimote",
+	    major:1,
+	    minor:2
+	});
+	Titanium.API.info("Start How Beacon: "+isStarted);
+*/
